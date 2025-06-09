8c3d4839a14e14d129467ff8411004b06172afb6
Abdallah Zidano <eng.zidano@yahoo.com>
2025-06-10 00:44:29 +0300

    add arabic letters images ✅

--- lib/models/arabic_alphabet_model.dart
+++ lib/models/arabic_alphabet_model.dart
@@ -1,37 +1,154 @@
 class ArabicAlphabetModel {
   final String letter;
   final String example;
+  final String imagePath;
 
-  const ArabicAlphabetModel({required this.letter, required this.example});
+  const ArabicAlphabetModel({
+    required this.letter,
+    required this.example,
+    required this.imagePath,
+  });
 
   static const List<ArabicAlphabetModel> letters = [
-    ArabicAlphabetModel(letter: 'أ', example: 'أرنب'),
-    ArabicAlphabetModel(letter: 'ب', example: 'باب'),
-    ArabicAlphabetModel(letter: 'ت', example: 'تاج'),
-    ArabicAlphabetModel(letter: 'ث', example: 'ثوب'),
-    ArabicAlphabetModel(letter: 'ج', example: 'جمل'),
-    ArabicAlphabetModel(letter: 'ح', example: 'حوت'),
-    ArabicAlphabetModel(letter: 'خ', example: 'خبز'),
-    ArabicAlphabetModel(letter: 'د', example: 'دب'),
-    ArabicAlphabetModel(letter: 'ذ', example: 'ذئب'),
-    ArabicAlphabetModel(letter: 'ر', example: 'رمان'),
-    ArabicAlphabetModel(letter: 'ز', example: 'زرافة'),
-    ArabicAlphabetModel(letter: 'س', example: 'سمك'),
-    ArabicAlphabetModel(letter: 'ش', example: 'شمس'),
-    ArabicAlphabetModel(letter: 'ص', example: 'صقر'),
-    ArabicAlphabetModel(letter: 'ض', example: 'ضفدع'),
-    ArabicAlphabetModel(letter: 'ط', example: 'طاووس'),
-    ArabicAlphabetModel(letter: 'ظ', example: 'ظبي'),
-    ArabicAlphabetModel(letter: 'ع', example: 'عصفور'),
-    ArabicAlphabetModel(letter: 'غ', example: 'غزال'),
-    ArabicAlphabetModel(letter: 'ف', example: 'فيل'),
-    ArabicAlphabetModel(letter: 'ق', example: 'قرد'),
-    ArabicAlphabetModel(letter: 'ك', example: 'كلب'),
-    ArabicAlphabetModel(letter: 'ل', example: 'ليمون'),
-    ArabicAlphabetModel(letter: 'م', example: 'موز'),
-    ArabicAlphabetModel(letter: 'ن', example: 'نمر'),
-    ArabicAlphabetModel(letter: 'ه', example: 'هدهد'),
-    ArabicAlphabetModel(letter: 'و', example: 'ورد'),
-    ArabicAlphabetModel(letter: 'ي', example: 'يمامة'),
+    ArabicAlphabetModel(
+      letter: 'أ',
+      example: 'أرنب',
+      imagePath: 'assets/images/arabic/a.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ب',
+      example: 'باب',
+      imagePath: 'assets/images/arabic/b.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ت',
+      example: 'تاج',
+      imagePath: 'assets/images/arabic/t.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ث',
+      example: 'ثوب',
+      imagePath: 'assets/images/arabic/th.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ج',
+      example: 'جمل',
+      imagePath: 'assets/images/arabic/g.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ح',
+      example: 'حوت',
+      imagePath: 'assets/images/arabic/h.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'خ',
+      example: 'خبز',
+      imagePath: 'assets/images/arabic/kh.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'د',
+      example: 'دب',
+      imagePath: 'assets/images/arabic/d.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ذ',
+      example: 'ذئب',
+      imagePath: 'assets/images/arabic/z.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ر',
+      example: 'رمان',
+      imagePath: 'assets/images/arabic/r.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ز',
+      example: 'زرافة',
+      imagePath: 'assets/images/arabic/zz.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'س',
+      example: 'سمك',
+      imagePath: 'assets/images/arabic/s.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ش',
+      example: 'شمس',
+      imagePath: 'assets/images/arabic/sh.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ص',
+      example: 'صقر',
+      imagePath: 'assets/images/arabic/ss.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ض',
+      example: 'ضفدع',
+      imagePath: 'assets/images/arabic/dd.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ط',
+      example: 'طاووس',
+      imagePath: 'assets/images/arabic/tt.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ظ',
+      example: 'ظبي',
+      imagePath: 'assets/images/arabic/zzz.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ع',
+      example: 'عصفور',
+      imagePath: 'assets/images/arabic/aa.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'غ',
+      example: 'غزال',
+      imagePath: 'assets/images/arabic/ph.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ف',
+      example: 'فيل',
+      imagePath: 'assets/images/arabic/f.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ق',
+      example: 'قرد',
+      imagePath: 'assets/images/arabic/q.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ك',
+      example: 'كلب',
+      imagePath: 'assets/images/arabic/k.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ل',
+      example: 'ليمون',
+      imagePath: 'assets/images/arabic/l.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'م',
+      example: 'موز',
+      imagePath: 'assets/images/arabic/m.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ن',
+      example: 'نمر',
+      imagePath: 'assets/images/arabic/n.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ه',
+      example: 'هدهد',
+      imagePath: 'assets/images/arabic/hh.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'و',
+      example: 'ورد',
+      imagePath: 'assets/images/arabic/w.jpeg',
+    ),
+    ArabicAlphabetModel(
+      letter: 'ي',
+      example: 'يمامة',
+      imagePath: 'assets/images/arabic/y.jpeg',
+    ),
   ];
 }
